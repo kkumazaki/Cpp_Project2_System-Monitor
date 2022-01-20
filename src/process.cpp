8abc806da3e50fa977bb7a4b45d56043f25570be
kkumazaki <kenta.kumazaki@woven-planet.global>
2022-01-20 16:59:19 +0900

    Fix Feedback#2 points.

--- src/process.cpp
+++ src/process.cpp
@@ -29,7 +29,8 @@ string Process::Ram() { return LinuxParser::Ram(Process::Pid()); }
 string Process::User() { return LinuxParser::User(Process::Pid()); }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return LinuxParser::UpTime(Process::Pid()) ; }
+//long int Process::UpTime() { return LinuxParser::UpTime(Process::Pid()) ; }
+long int Process::UpTime() { return LinuxParser::UpTime() - LinuxParser::UpTime(Process::Pid()) ; }// change after Feedback#2
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
--- src/processor.cpp
+++ src/processor.cpp
@@ -8,14 +8,18 @@ float Processor::Utilization() {
   std::string key;
   std::string val1, val2, val3, val4, val5, val6, val7, val8, val9, val10;
   float idle, nonidle, utilization; 
-  std::ifstream filestream(LinuxParser::kProcDirectory + LinuxParser::kMeminfoFilename);
+  //std::ifstream filestream(LinuxParser::kProcDirectory + LinuxParser::kMeminfoFilename);
+  std::ifstream filestream(LinuxParser::kProcDirectory + LinuxParser::kStatFilename); // change after Feedback#2
   if (filestream.is_open()) {
     while (std::getline(filestream, line)) {
       std::istringstream linestream(line);
       while (linestream >> key >> val1 >> val2 >> val3 >> val4 >> val5 >> val6 >> val7 >> val8 >> val9 >> val10) {
-        if (key == "cpu")
-          idle = std::stof(val4 + val5); // idle = val4 (idle) + val5 (iowait)
-          nonidle = std::stof(val1 + val2 + val3 + val6 + val7 + val8 + val9 + val10);
+        //if (key == "cpu")
+        if (key == "cpu"){ // change after Feedback#2
+          //idle = std::stof(val4 + val5); // idle = val4 (idle) + val5 (iowait)
+          idle = std::stof(val4) + std::stof(val5); // idle = val4 (idle) + val5 (iowait)
+          nonidle = std::stof(val1) + std::stof(val2) + std::stof(val3) + std::stof(val6) + std::stof(val7) + std::stof(val8) + std::stof(val9) + std::stof(val10);
+        } // change after Feedback#2
       }
     }
   }
